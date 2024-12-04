b6a49806d0911cba90195c46bdc84d95017296da
Balázs Orbán <285574+orbanbalage@users.noreply.github.com>
2024-12-05 00:26:16 +0100

    Day 04 both

--- Day04/main.py
+++ Day04/main.py
@@ -32,6 +32,19 @@ S.S.S.S.SS
 
 # oh actually it's easy - we only check for the word, if the element we're iterating on == "X", and then we check all the additional three conditionals for letters "MAS" 
 
+input_sample2 = """.M.S......
+..A..MSMS.
+.M.S.MAA..
+..A.ASMSM.
+.M.S.M....
+..........
+S.S.S.S.S.
+.A.A.A.A..
+M.M.M.M.M.
+.........."""
+
+# sample 2 should return 9
+
 #for line in input_sample:
 #	print(line)
 
@@ -49,7 +62,7 @@ print(input_arr)
 
 x, y = 0, 0
 
-for line in input_text.split("\n"):
+for line in input_text.split("\n"): #replace to input_sample or input_sample2 for testing...
 	#print(line)
 	for char in line:
 		#print(char, x, y)
@@ -83,7 +96,7 @@ for x in range(0, input_height):
 		if(x >= 3 and input_arr[x][y] == "X" and input_arr[x-1][y] == "M" and input_arr[x-2][y] == "A" and input_arr[x-3][y] == "S"):
 			xmas_count += 1
 #4,5PM:
-		if(x + 3 < input_height and y + 3 <= input_width and input_arr[x][y] == "X" and input_arr[x+1][y+1] == "M" and input_arr[x+2][y+2] == "A" and input_arr[x+3][y+3] == "S"):
+		if(x + 3 < input_height and y + 3 < input_width and input_arr[x][y] == "X" and input_arr[x+1][y+1] == "M" and input_arr[x+2][y+2] == "A" and input_arr[x+3][y+3] == "S"):
 			xmas_count += 1
 #10,5PM:
 		if(y >= 3 and x >= 3 and input_arr[x][y] == "X" and input_arr[x-1][y-1] == "M" and input_arr[x-2][y-2] == "A" and input_arr[x-3][y-3] == "S"):
@@ -98,4 +111,41 @@ for x in range(0, input_height):
 
 print(xmas_count)
 
+#################
+
+# M.S
+# .A.
+# M.S
+
+# 4 combo. Counter-Clockwise from bottom right:
+
+# 4,5 +  1,5	SSMM
+# 7,5 +  4,5	SMMS
+#10,5 +  7,5	MMSS
+# 1,5 + 10,5	MSSM
+
+mas_count = 0
+
+#we can express each vector, like the clock. 3PM means horizontal, LTR.
+
+# we can iterate just once!!!:
+for x in range(0, input_height):
+	for y in range(0, input_width):
+		#print(input_arr[x][y])
+# and write the conditions here for each clock:
+#4,5PM:
+		if(x + 1 < input_height and y + 1 < input_width and x - 1 >= 0 and y - 1 >= 0 and  input_arr[x][y] == "A" and input_arr[x+1][y+1] == "S" and input_arr[x-1][y+1] == "S" and input_arr[x-1][y-1] == "M" and input_arr[x+1][y-1] == "M"):
+			mas_count += 1
+#10,5PM:
+		if(x + 1 < input_height and y + 1 < input_width and x - 1 >= 0 and y - 1 >= 0 and  input_arr[x][y] == "A" and input_arr[x+1][y+1] == "S" and input_arr[x-1][y+1] == "M" and input_arr[x-1][y-1] == "M" and input_arr[x+1][y-1] == "S"):
+			mas_count += 1
+#7,5PM:
+		if(x + 1 < input_height and y + 1 < input_width and x - 1 >= 0 and y - 1 >= 0 and  input_arr[x][y] == "A" and input_arr[x+1][y+1] == "M" and input_arr[x-1][y+1] == "M" and input_arr[x-1][y-1] == "S" and input_arr[x+1][y-1] == "S"):
+			mas_count += 1
+#1,5PM:
+		if(x + 1 < input_height and y + 1 < input_width and x - 1 >= 0 and y - 1 >= 0 and  input_arr[x][y] == "A" and input_arr[x+1][y+1] == "M" and input_arr[x-1][y+1] == "S" and input_arr[x-1][y-1] == "S" and input_arr[x+1][y-1] == "M"):
+			mas_count += 1
+
+
+print(mas_count)
 
